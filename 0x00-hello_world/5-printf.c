a8e88dd19e2c96630e32cdc7295c3e96f0d4438d
KgololoSydney <setsetsesydney@gmail.com>
2023-02-16 14:23:40 -0800

    Comit number 4 and 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,10 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- *
  */
 
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+    return (0);
 }
