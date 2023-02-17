585573866e5b9fdd6769a49ea250dea1f87fa0cc
KgololoSydney <setsetsesydney@gmail.com>
2023-02-17 05:15:56 -0800

    Commit 1 and 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,22 @@
-dgfvhdgfh
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Return: returns 0
+ */
+int main(void)
+{
+	char alphabet, newline;
+
+	alphabet = 'a';
+	newline = '\n';
+	while (alphabet <= 'z')
+	{
+		putchar (alphabet);
+		alphabet++;
+	}
+	putchar (newline);
+
+	return (0);
+}
+
