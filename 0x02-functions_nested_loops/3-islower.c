9196b61f4f02bb8d3dbd6f7f0ec0f92376692354
KgololoSydney <setsetsesydney@gmail.com>
2023-02-23 03:03:42 -0800

    Commited task 0

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
   * _islower - Entry point
   * Description: Checks for lowercase character.
   * @c: Contains value to be compared
   * Return: Always 0 (Success)
   */ 
+
 int _islower(int c)
 {
 	/* ASCII value of lower case letters is between 97 & 122 */
@@ -12,6 +14,7 @@ int _islower(int c)
 	{
 		return (1);
 	}
+
 	else
 	{
 		return (0);
