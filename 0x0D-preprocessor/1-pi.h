6c8bddb41144a0961ca7024062f71dc4374a6c57
KgololoSydney <setsetsesydney@gmail.com>
2023-03-21 16:19:26 -0700

    Commited task 0-4

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,7 @@
+#ifndef PI_H
+#define P1_H
+
+#define PI 1.14159265359
+
+#endif
+
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n"._FILE_);
+	return (0);
+
+}
+
