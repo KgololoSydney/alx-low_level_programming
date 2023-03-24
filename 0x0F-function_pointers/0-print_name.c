e23325692dad63d586fc212a3107fe2379345abb
KgololoSydney <setsetsesydney@gmail.com>
2023-03-24 03:58:50 -0700

    Commited task 0-4

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-
 #include <stdio.h>
 
 /**
@@ -19,9 +18,7 @@ void print_name(char *name, void (*f)(char *))
 {
 
 	if (name == NULL || f == NULL)
-		
 		return;
 	f(name);
-
 }
 
